ba0aabbadab18c7aacee12322f2fd4f4f4926d0d
Michael Neumann <mne@micneu.net>
2021-04-15 17:35:47 +0200

    mein erste go programm

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/micneu72/gocat
+
+go 1.16
--- main.go
+++ main.go
@@ -1,1 +1,30 @@
+package main
 
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+func main() {
+	for _, fname := range os.Args[1:] {
+		fmt.Println(fname)
+
+		if len(os.Args) == 1 {
+			fmt.Println("Mindestens eine Datei als Parameter erwartet")
+			os.Exit(1)
+		}
+		f, err := os.Open(fname)
+		if err != nil {
+			log.Printf("Fehler beim Öffnen der Datei: %s/n%s", fname, err)
+			f.Close()
+			continue
+		}
+		_, err = io.Copy(os.Stdout, f)
+		if err != nil {
+			log.Printf("Fehler bei der Ausgabe von %s\n%s", fname, err)
+		}
+		f.Close()
+	}
+}
